0cf1ba1a959ed6c0fb82df4ab5986eb723f11e66
Ryan Garrett <ryangar46@gmail.com>
2022-06-20 22:40:17 -0400

    Add missing texture warning to validation

--- resource_pack_packer/validation.py
+++ resource_pack_packer/validation.py
@@ -1,12 +1,55 @@
 import json
 import logging
 import os
+from enum import Enum
+from functools import singledispatch
 from glob import glob
 from multiprocessing import Pool
+from typing import overload, Optional
 
 import jsonschema
 
 from resource_pack_packer.console import add_to_logger_name
+
+
+class AssetType(Enum):
+    BLOCKSTATE = "blockstate"
+    MODEL = "model"
+    SOUND_INDEX = "sound_index"
+
+    @staticmethod
+    def get_path(asset_type: "AssetType") -> str:
+        """
+        Gets the relative path of the asset from the asset folder.
+
+        :param asset_type: The type of asset
+        :return: The relative path of the asset
+        """
+
+        match asset_type:
+            case AssetType.BLOCKSTATE:
+                return os.path.join("blockstates", "**")
+            case AssetType.MODEL:
+                return os.path.join("models", "**")
+            case AssetType.SOUND_INDEX:
+                return "sounds.json"
+
+    @staticmethod
+    def get_schema_path(asset_type: "AssetType") -> str:
+        """
+        Get the path of the schema.
+
+        :param asset_type: The type of asset
+        :return: The path of the schema
+        """
+
+        match asset_type:
+            case AssetType.BLOCKSTATE:
+                return os.path.join("minecraft", "assets", "blockstates", "blockstate.schema.json")
+            case AssetType.MODEL:
+                return os.path.join("minecraft", "assets", "models", "model.schema.json")
+            case AssetType.SOUND_INDEX:
+                return os.path.join("minecraft", "assets", "sounds.schema.json")
 
 
 def validate(pack: str, logger_name: str):
@@ -14,52 +57,71 @@ def validate(pack: str, logger_name: str):
 
     assets_dir = os.path.join(pack, "assets", "*")
 
+    # Blockstates
     logger.info("Validating blockstates...")
-    validate_assets(os.path.join(assets_dir, "blockstates", "**"), "json",
-                    "minecraft/assets/blockstates/blockstate.schema.json", logger)
+    validate_assets(assets_dir, AssetType.BLOCKSTATE, "json", logger)
     logger.info("Validated blockstates.")
 
+    # Models
     logger.info("Validating models...")
-    validate_assets(os.path.join(assets_dir, "models", "**"), "json", "minecraft/assets/models/model.schema.json",
-                    logger)
+    validate_assets(assets_dir, AssetType.MODEL, "json", logger)
     logger.info("Validated models.")
 
-    validate_asset(os.path.join(assets_dir, "sounds.json"), get_schema("minecraft/assets/models/model.schema.json"),
-                   logger)
+    # Sound index
+    validate_asset(os.path.join(assets_dir, AssetType.get_path(AssetType.SOUND_INDEX)), AssetType.SOUND_INDEX, logger)
 
 
-def get_schema(schema: str) -> dict:
-    with open(os.path.join("schema", schema), "r") as raw_schema:
+def get_schema(asset_type: AssetType) -> dict:
+    with open(os.path.join("schema", AssetType.get_schema_path(asset_type)), "r") as raw_schema:
         parsed_schema = json.load(raw_schema)
     return parsed_schema
 
 
-def validate_asset(file: str, schema: dict, logger: logging.Logger) -> bool:
+@singledispatch
+def validate_asset(assets_dir: str, asset_type: AssetType, logger: logging.Logger, schema: Optional[dict] = None) -> bool:
+    if schema is None:
+        schema = get_schema(asset_type)
+
+    file = os.path.join(assets_dir)
+
     if os.path.exists(file):
         with open(file, "r") as raw_data:
             data = json.load(raw_data)
 
         try:
             jsonschema.validate(data, schema)
-            return True
         except jsonschema.ValidationError as e:
             logger.warning(f"{file} didn't match schema:\n{e.message}")
             return False
+
+        # Asset specific checks
+        match asset_type:
+            case AssetType.MODEL:
+                # Elements
+                if "elements" in data:
+                    for element in data["elements"]:
+                        # Faces
+                        if "faces" in element:
+                            for face in element["faces"].values():
+                                # Texture
+                                if "texture" in face and face["texture"] == "#missing":
+                                    logger.warning(f"Missing texture in: {file}")
+
     return True
 
 
-def validate_assets(folder: str, extension: str, schema: str, logger: logging.Logger):
-    files = glob(folder, recursive=True)
-    parsed_schema = get_schema(schema)
+def validate_assets(asset_dir: str, asset_type: AssetType, extension: str, logger: logging.Logger):
+    files = glob(os.path.join(asset_dir, AssetType.get_path(asset_type)), recursive=True)
+    parsed_schema = get_schema(asset_type)
     filtered_files = []
 
     for file in files:
         if os.path.isfile(file) and file.endswith(f".{extension}"):
-            filtered_files.append([file, parsed_schema, logger])
+            filtered_files.append([file, asset_type, logger, parsed_schema])
 
     with Pool(processes=os.cpu_count()) as p:
         p.map(_validate_assets, filtered_files)
 
 
 def _validate_assets(arg: list):
-    validate_asset(arg[0], arg[1], arg[2])
+    validate_asset(arg[0], arg[1], arg[2], arg[3])
